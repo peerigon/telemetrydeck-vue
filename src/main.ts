ede9b7fe39bcd66194bc4d7d48269a87361f98a9
Sally Mclean <smclean17@gmail.com>
2024-07-12 20:41:34 +0200

    use env file

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import TelementryDeckPlugin from './plugin'
 
 const app = createApp(App)
 app.use(TelementryDeckPlugin, {
-  appID: '03158F6D-5EAE-4713-B148-69E003476E9A',
+  appID: import.meta.env.VITE_TELEMENTRYDECK_APP_ID,
   testMode: true,
 })
 
