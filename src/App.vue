e80ec8d7d6595ea8eb94a31d8a48cf04b40dcfe5
Sally Mclean <smclean17@gmail.com>
2024-07-15 13:12:21 +0200

    add tests for passing options

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,43 @@
       timestamp: new Date().toISOString(),
     });
   };
+  const buttonSignalClickWithOptions = () => {
+    signal('example_signal_event_name', {
+      custom_data: 'other_data',
+      timestamp: new Date().toISOString(),
+    }, {
+      testMode: true,
+      clientUser: 'other_user',
+      appID: 'other_app_id',
+    }
+  );
+  };
+  const buttonQueueClickWithOptions = () => {
+    queue('example_queue_event_name', {
+      custom_data: 'other_data',
+      timestamp: new Date().toISOString(),
+    },
+    {
+      testMode: true,
+      clientUser: 'other_user',
+      appID: 'other_app_id',
+    });
+  };
 </script>
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
+    <div>
     <button id="btnSignalClick" @click="buttonSignalClick">Log a click with signal</button>
     <button id="btnQueueClick" @click="buttonQueueClick">Log a click with queue</button>
     <button id="btnSetClient" @click="changeClientUserClick">Change user</button>
+    </div>
+    <div>
+      <button id="btnSignalClickWithOptions" @click="buttonSignalClickWithOptions">Log a click with signal with Options</button>
+      <button id="btnQueueClickWithOptions" @click="buttonQueueClickWithOptions">Log a click with queue with Options</button>
+    </div>
   </div>
   
 </template>
